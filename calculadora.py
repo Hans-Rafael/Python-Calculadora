8866b7f6f6bcbab2caa32b39b1d3312a0fbb9765
hans-rafael <hansvekoni@gmail.com>
2026-03-15 16:40:44 -0300

    calculadora robusta added

--- calculadora.py
+++ calculadora.py
@@ -42,3 +42,49 @@ elif operacion == '/':
 print("El resultado es:", resultado)
 
 #### Calculador completa con funciones######
+def pedir_numero(mensaje):
+    while True:
+        try:
+            numero = float(input(mensaje))
+            return numero
+        except ValueError:
+            print("Debe ingresar un numero valido.")
+
+
+def pedir_operacion():
+    while True:
+        operacion = input("Ingrese la operacion (+, -, *, /): ")
+        if operacion in ['+', '-', '*', '/']:
+            return operacion
+        else:
+            print("Operacion no valida.")
+
+
+def calcular(num1, num2, operacion):
+
+    if operacion == '+':
+        return num1 + num2
+    elif operacion == '-':
+        return num1 - num2
+    elif operacion == '*':
+        return num1 * num2
+    elif operacion == '/':
+        return num1 / num2
+
+
+print("Calculadora basica")
+
+num1 = pedir_numero("Ingrese el primer numero: ")
+operacion = pedir_operacion()
+
+while True:
+    num2 = pedir_numero("Ingrese el segundo numero: ")
+
+    if operacion == '/' and num2 == 0:
+        print("No se puede dividir por 0.")
+    else:
+        break
+
+resultado = calcular(num1, num2, operacion)
+
+print("El resultado es:", resultado)
